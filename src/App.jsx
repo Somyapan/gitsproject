bc041cc76c35537d7a38603aa03d0e972441c3d0
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-27 12:22:56 +0530

    Added different routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import SimpleCarousel from "./components/SimpleCarousel";
-import Patner from "./Patner";
-import Difference from "./components/Difference";
+import { Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExtraPage from "./pages/ExtraPage";
 
 function App() {
   return (
-    <div className="App">
-      <SimpleCarousel />
-      <Difference />
-      <Patner />
+    <div className="w-full">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/:slug" element={<ExtraPage />} />
+      </Routes>
       <Footer />
     </div>
   );
