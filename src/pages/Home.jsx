12ffe9d2d211474cef5f47652e8cb9a59752a601
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-27 12:21:59 +0530

    Created Home page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Patner from "../components/Patner";
+import Difference from "../components/Difference";
+import SimpleCarousel from "../components/SimpleCarousel";
+
+const Home = () => {
+  return (
+    <>
+      <SimpleCarousel />
+      <Difference />
+      <Patner />
+    </>
+  );
+};
+
+export default Home;
