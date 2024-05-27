1519b1834b1685acc830d09bc8c33520672be10d
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-27 12:21:30 +0530

    Added custom animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blink: {
+          "0%, 33.33%": { backgroundColor: "#1ebb24" },
+          "33.33%, 66.66%": { backgroundColor: "#0c354a" },
+          "66.66%, 100%": { backgroundColor: "#fe6b04" },
+        },
+      },
+      animation: {
+        "blink-link": "blink 1s ease infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
